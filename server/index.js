e006e7755cedb5d79281b3a33ea07039fed1cc15
Hamish <chan.hamish@gmail.com>
2020-09-23 11:14:44 +0100

    feat: initial server setup

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+const cors = require("cors");
+const connectDB = require('./config/db');
+
+//load config
+dotenv.config({ path: './config/config.env' });
+
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// set up cors to allow us to accept requests from our client
+app.use(
+  cors({
+    origin: "http://localhost:1234", // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true // allow session cookie from browser to pass through
+  })
+);
+
+
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+const CLIENT_URL = 'http://localhost:1234/';
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/auth/login/failed"
+  })
+);
+
+// when login is successful, retrieve user info
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.json({
+      success: true,
+      message: "user has successfully authenticated",
+      user: req.user,
+      cookies: req.cookies
+    });
+  }
+});
+
+// when login failed, send failed msg
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "user failed to authenticate."
+  });
+});
+
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
+
+module.exports = router;
