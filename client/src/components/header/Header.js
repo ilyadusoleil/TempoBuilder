b8d211dfdfda0eaea521926ef3757890aee464b8
Hamish <chan.hamish@gmail.com>
2020-09-24 14:11:21 +0100

    feat: profile picture in corner

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,43 +1,59 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { css } from '@emotion/core';
+
+import Context from '../../Context';
+import { HEADER_HEIGHT } from '../../constants';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Header = ({setIsSideBarOpen}) => {
+const PIC_MARGIN = 10;
+
+const Header = ({ setIsSideBarOpen }) => {
+  const ctx = useContext(Context);
   return (
-<div
-          css={css`
-            display: flex;
-            background: peru;
-            height: 50px;
-            align-items: center;
-            margin-bottom: 20px;
-            padding-left: 15px;
-          `}
-        >
-          <FontAwesomeIcon
-            css={css`
-              cursor: pointer;
-              &:hover {
-                transform: scale(1.3);
-              }
-            `}
-            onClick={() => setIsSideBarOpen(true)}
-            color="#black"
-            size="1x"
-            icon={faBars}
-          />
-          <div
-            css={css`
-              font-size: 25px;
-              font-weight: 800;
-              margin-left: 15px;
-            `}
-          >
-            Tempo Builder
-          </div>
-        </div>
+    <div
+      css={css`
+        display: flex;
+        background: peru;
+        height: ${HEADER_HEIGHT}px;
+        align-items: center;
+        margin-bottom: 20px;
+        padding-left: 15px;
+      `}
+    >
+      <FontAwesomeIcon
+        css={css`
+          cursor: pointer;
+          &:hover {
+            transform: scale(1.3);
+          }
+        `}
+        onClick={() => setIsSideBarOpen(true)}
+        color="#black"
+        size="1x"
+        icon={faBars}
+      />
+      <div
+        css={css`
+          font-size: 25px;
+          font-weight: 800;
+          margin-left: 15px;
+        `}
+      >
+        Tempo Builder
+      </div>
+      <img
+        css={css`
+          margin: ${PIC_MARGIN}px;
+          margin-left: auto;
+          border-radius: ${(HEADER_HEIGHT - PIC_MARGIN * 2) / 2}px;
+        `}
+        alt="profile"
+        src={ctx.state.user.image}
+        height={HEADER_HEIGHT - PIC_MARGIN * 2}
+      />
+    </div>
   );
 };
 
