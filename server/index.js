4b03803c4ef4ec08e5feb64ef856095d2fb9faf6
Hamish <chan.hamish@gmail.com>
2020-09-29 12:14:49 +0100

    feat: copy dist client to server and server static

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session)
-const cors = require("cors");
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 //load config
 dotenv.config({ path: './config/config.env' });
@@ -21,25 +22,26 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(
   session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // set up cors to allow us to accept requests from our client
 app.use(
   cors({
-    origin: "http://localhost:1234", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
+    origin: 'http://localhost:1234', // allow to server to accept request from different origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow session cookie from browser to pass through
   })
 );
 
@@ -48,6 +50,10 @@ app.use(express.json());
 app.use('/auth', require('./routes/auth'));
 app.use('/api', require('./routes/images'));
 app.use('/piece', require('./routes/pieces'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
